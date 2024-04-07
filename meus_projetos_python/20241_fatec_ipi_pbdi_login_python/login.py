baeb14d19da23b23c82f118430a25c73c38c63b8
William Santos <william.santos86@fatec.sp.gov.br>
2024-04-07 01:20:11 -0300

    feat(ap07):Função inserir implementada no menu

--- meus_projetos_python/20241_fatec_ipi_pbdi_login_python/login.py
+++ meus_projetos_python/20241_fatec_ipi_pbdi_login_python/login.py
@@ -28,13 +28,33 @@ def existe(usuario):
             result = cursor.fetchone()
             return result != None
 
+
+def insert(usuario):
+    # Conecta BD
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="20241_pessoal_db_login",
+        user="postgres",
+        password="123456"
+    ) as conexão:
+        # obter cursor
+        with conexão.cursor() as cursor:
+            # executa comando
+            cursor.execute(
+                'INSERT INTO tb_usuario(login, senha) VALUES(%s, %s) RETURNING login', (usuario.login, usuario.senha))
+            # obter resultado
+            result = cursor.fetchone()
+            # return True if result != None else False
+            return result != None
+
 # def teste():
 # print(existe(Usuario('admin', 'admin')))
 # teste()
 
 
 def menu():
-    texto = '0-Sair\n1-Login\n2-Logout\n'
+    texto = '0-Sair\n1-Login\n2-Logout\n3-Criar Usuário'
     usuario = None
     op = int(input(texto))
     while op != 0:
@@ -46,8 +66,15 @@ def menu():
         elif op == 2:
             usuario = None
             print('Logout realizado com sucesso')
+        elif op == 3:
+            login = input('Login:')
+            senha = input('Senha:')
+            usuario = Usuario(login, senha)
+            insert(usuario)
+            print('Usuário criado' if insert(
+                usuario) else 'Usuário não criado')
         else:
-            print('digite a opção válida')
+            print('digite a opção válida\n')
         op == int(input(texto))
 
 
